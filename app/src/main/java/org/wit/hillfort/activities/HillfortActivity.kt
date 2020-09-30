7f0fa2bd5eee8f0a4779d59c92c22583b052f1ac
jmccannwit <johnmccann02@yahoo.co.uk>
2020-09-30 09:48:38 +0100

    second commit baseline features

---
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -0,0 +1,71 @@
+package org.wit.hillfort.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.models.HillfortModel
+
+
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+  var hillfort = HillfortModel()
+  lateinit var app: MainApp
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_hillfort)
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
+    info("Hillfort Activity started..")
+
+    app = application as MainApp
+
+    var edit = false
+
+    if (intent.hasExtra("hillfort_edit")) {
+      edit = true
+      hillfort = intent.extras?.getParcelable<HillfortModel>("hillfort_edit")!!
+      hillfortTitle.setText(hillfort.title)
+      description.setText(hillfort.description)
+      btnAdd.setText(R.string.save_hillfort)
+    }
+
+    btnAdd.setOnClickListener() {
+      hillfort.title = hillfortTitle.text.toString()
+      hillfort.description = description.text.toString()
+      if (hillfort.title.isEmpty()) {
+        toast(R.string.enter_hillfort_title)
+      } else {
+        if (edit) {
+          app.hillforts.update(hillfort.copy())
+        } else {
+          app.hillforts.create(hillfort.copy())
+        }
+      }
+      info("add Button Pressed: $hillfortTitle")
+      setResult(AppCompatActivity.RESULT_OK)
+      finish()
+    }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_hillfort, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> {
+        finish()
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
+}
---
+++ app/src/main/java/org/wit/hillfort/models/HillfortModel.kt
@@ -0,0 +1,9 @@
+package org.wit.hillfort.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class HillfortModel(var id: Long = 0,
+                          var title: String = "",
+                          var description: String = "") : Parcelable
