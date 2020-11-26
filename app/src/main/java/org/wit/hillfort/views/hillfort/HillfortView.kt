65fa0154ca042258c54fb6d49c1b94e8a8939c79
jmccannwit <johnmccann02@yahoo.co.uk>
2020-11-26 14:27:02 +0000

    Additional labels and up support

--- app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.toast
@@ -12,29 +13,31 @@ import org.wit.hillfort.helpers.readImageFromPath
 import org.wit.hillfort.models.HillfortModel
 import org.wit.hillfort.views.BaseView
 
-
 class HillfortView : BaseView(), AnkoLogger {
 
     lateinit var presenter: HillfortPresenter
     var hillfort = HillfortModel()
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
-        super.init(toolbarAdd)
 
-
-        presenter = initPresenter (HillfortPresenter(this)) as HillfortPresenter
+        super.init(toolbarAdd, true);
 
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync {
-            presenter.doConfigureMap(it)
+            map = it
+            presenter.doConfigureMap(map)
             it.setOnMapClickListener { presenter.doSetLocation() }
         }
 
-        chooseImage.setOnClickListener { presenter.doSelectImage() }
+        presenter = initPresenter (HillfortPresenter(this)) as HillfortPresenter
 
-
+        chooseImage.setOnClickListener {
+            presenter.cacheHillfort(hillfortTitle.text.toString(), description.text.toString(), rating.text.toString())
+            presenter.doSelectImage()
+        }
     }
 
     override fun showHillfort(hillfort: HillfortModel) {
@@ -51,6 +54,7 @@ class HillfortView : BaseView(), AnkoLogger {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_hillfort, menu)
+        if (presenter.edit) menu.getItem(0).setVisible(true)
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -63,15 +67,10 @@ class HillfortView : BaseView(), AnkoLogger {
                 if (hillfortTitle.text.toString().isEmpty()) {
                     toast(R.string.enter_hillfort_title)
                 } else {
-                    presenter.doAddOrSave(hillfortTitle.text.toString(), description.text.toString(),rating.text.toString())
+                    presenter.doAddOrSave(hillfortTitle.text.toString(), description.text.toString(), rating.text.toString())
                 }
             }
-
             R.id.item_cancel -> {
-                finish()
-            }
-
-            R.id.item_back -> {
                 finish()
             }
         }
